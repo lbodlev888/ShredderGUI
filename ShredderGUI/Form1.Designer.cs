22b31afc2d555bc4fd3e55d6fc56e52d15f7b4c0
Bodlev Laurentiu <lbodlev888@gmail.com>
2024-03-26 20:22:34 +0200

    Initial commit

---
+++ ShredderGUI/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ShredderGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(161, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Delete after shredding";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(276, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nr times";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(360, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(81, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Select file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(569, 372);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 440);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Shredder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ShredderGUI/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Threading;
+using System.IO.MemoryMappedFiles;
+
+namespace ShredderGUI
+{
+    public partial class Form1 : Form
+    {
+        string filePath = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            for(int i = 0; i < textBox2.Text.Length; i++)
+            {
+                if (textBox2.Text[i] < '0' || textBox2.Text[i] > '9')
+                {
+                    textBox2.Text = textBox2.Text.Remove(i, 1);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                int BUFFER_SIZE = (int)stream.Length;
+                using(var rng = new RNGCryptoServiceProvider())
+                {
+                    for(int i = 1; i <= int.Parse(textBox2.Text); i++)
+                    {
+                        byte[] randomData = new byte[BUFFER_SIZE];
+                        rng.GetBytes(randomData);
+                        listBox1.Items.Add($"Shredding {i}/{textBox2.Text} (" + Convert.ToBase64String(randomData).Substring(0, 10) + ")");
+                        stream.Write(randomData, 0, randomData.Length);
+                    }
+                }
+            }
+            /*//https://stackoverflow.com/questions/57017270/unable-to-read-video-file-greater-than-2gb
+            int BUFFER_SIZE = File.ReadAllBytes(filePath).Length;
+            using(var rng = new RNGCryptoServiceProvider())
+            {
+                for(int i = 1; i <= int.Parse(textBox2.Text); i++)
+                {
+                    byte[] randomData = new byte[BUFFER_SIZE];
+                    rng.GetBytes(randomData);
+                    listBox1.Items.Add($"Shredding {i}/{textBox2.Text} (" + Convert.ToBase64String(randomData).Substring(0, 10) + ")");
+                    File.WriteAllBytes(filePath, randomData);
+                }
+            }*/
+            if(checkBox1.Checked)
+            {
+                string dirPath = Path.GetDirectoryName(filePath) + "\\";
+                for (int i = filePath.Length; i > 0; i--)
+                {
+                    string final = dirPath;
+                    for (int j = 0; j < i; j++) final += "0";
+                    File.Move(filePath, final);
+                    filePath = final;
+                    listBox1.Items.Add("File renamed to " + final);
+                }
+                File.Delete(filePath);
+                listBox1.Items.Add(filePath + " The file was removed successfully");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                listBox1.Items.Add("File selected => " + filePath);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) listBox1.Items.Add("The file will be deleted");
+            else listBox1.Items.Add("The file won't be deleted");
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 0) listBox1.Items.Add("Number of iterations => " + textBox2.Text);
+        }
+    }
+}
