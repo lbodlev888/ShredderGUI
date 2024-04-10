f7dd4e1ee5801b916d9dae2b8a027b289b3c48e3
Bodlev Laurentiu <lbodlev888@gmail.com>
2024-04-10 08:38:43 +0300

    Added multiple files select feature

--- ShredderGUI/Form1.Designer.cs
+++ ShredderGUI/Form1.Designer.cs
@@ -82,6 +82,7 @@
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
             // 
             // button2
             // 
